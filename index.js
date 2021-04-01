891c17247b1f2bab72c240cc8b03fdc0b3b010df
bnula <bnulagnula@gmail.com>
2021-04-01 16:39:40 +0200

    added visuals to button presses

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let colorSequence = [];
 
 $(".btn").on("click", (e) => {
       color = e.target.id;
+      buttonPress(color, "player-pressed");
+      randomPress();
       console.log(color);
 });
 
@@ -12,9 +14,20 @@ $(document).on("keydown", () => {
       $(document).off("keydown");
 });
 
+function buttonPress (color, className) {
+      $(`#${color}`).toggleClass(className);
+      setTimeout( () => {
+            $(`#${color}`).toggleClass(className);
+      }, 100);
+}
+
 function randomPress () {
       let index = Math.random() * 4;
       index = Math.floor(index);
       let color = colors[index];
-      console.log(color);
+      setTimeout( () => {
+            buttonPress(color, "random-pressed");
+      }, 200);
+      colorSequence.push(color);
+      console.log(colorSequence);
 };
